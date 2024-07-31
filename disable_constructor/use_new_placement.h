eeb528c61ce436850eddfa8faf549e4a07a8e03e
Apribojimas <34028680+Apribojimas@users.noreply.github.com>
2024-07-31 07:54:33 +0300

    Create use_new_placement.h

---
+++ disable_constructor/use_new_placement.h
@@ -0,0 +1,13 @@
+  template <class T> class Indestructible {
+    alignas(T) unsigned char data_[sizeof(T)];  // using 'new placement' trick
+  public:
+    template <class... Ts> Indestructible(Ts&&... ts) noexcept { new (data_) T(std::forward<Ts>(ts)...); }
+    ~Indestructible() noexcept = default;
+
+    const T& value() const { return *reinterpret_cast<T*>(data_); }
+    T& value() { return *reinterpret_cast<T*>(data_); }
+    const T* get() const { return reinterpret_cast<T*>(data_); }
+    T* get() { return reinterpret_cast<T*>(data_); }
+    const T* operator->() const { return get(); }
+    T* operator->() { return get(); }
+  };
