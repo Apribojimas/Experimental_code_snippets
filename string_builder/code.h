91df70c6fd1f34ddfb094ca5072e43f7a179afa1
Apribojimas <34028680+Apribojimas@users.noreply.github.com>
2025-07-18 12:10:16 +0300

    Update code.h

--- string_builder/code.h
+++ string_builder/code.h
@@ -17,3 +17,30 @@ std::string join(const std::vector<std::string>& a_vec, const char* separator) {
       return str1.empty() ? str2 : str1 + separator + str2;
     });
 }
+
+// avoid re-allocations
+std::string join(const std::vector<std::string>& svec, const std::string& sepa) {
+	if (svec.empty())	return {};
+	/* calculate the overall size beforehand, to avoid re-allocations. */
+	size_t value_len =
+		std::accumulate(svec.cbegin(), svec.cend(), 0,
+				[](size_t size, const std::string& s) {
+					return size + s.size();
+				}) + (svec.size() - 1) * sepa.length();
+
+	std::string value;
+	value.reserve(value_len);
+
+	std::accumulate(svec.cbegin(), svec.cend(), std::ref(value),
+			[&](std::string& s1, const std::string& s2)->std::string& {
+				if (s1.empty())
+					s1 = s2;
+				else {
+					s1.append(sepa);
+					s1.append(s2);
+				}
+				return s1;
+			});
+
+	return value;
+}
