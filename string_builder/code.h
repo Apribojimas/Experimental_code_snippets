aa70e2c5997c420717bcc088559a70210e5c4902
Apribojimas <34028680+Apribojimas@users.noreply.github.com>
2025-07-18 12:06:43 +0300

    Update code.h

--- string_builder/code.h
+++ string_builder/code.h
@@ -9,3 +9,11 @@
       return os.str();
     }
   }
+
+// concatenates with separator
+std::string join(const std::vector<std::string>& a_vec, const char* separator) {
+  return std::accumulate(a_vec.begin(), a_vec.end(), std::string{},
+    [separator](const auto& str1, const auto& str2) {
+      return str1.empty() ? str2 : str1 + separator + str2;
+    });
+}
